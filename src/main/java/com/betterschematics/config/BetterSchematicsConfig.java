1fea7916d44324f436fa9f5b990966cd598c8114
pdnia <jeszczeraz8976@gmail.com>
2026-07-04 00:03:14 +0200

    Update BetterSchematicsConfig from workspace

--- src/main/java/com/betterschematics/config/BetterSchematicsConfig.java
+++ src/main/java/com/betterschematics/config/BetterSchematicsConfig.java
@@ -1,83 +1,20 @@
 package com.betterschematics.config;
 
-import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common>ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
+import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = "betterschematics", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = "betterschematics")
 public class BetterSchematicsConfig {
-
-    public static final ForgeConfigSpec SPEC;
-    
-    public static final ForgeConfigSpec.DoubleValue RENDER_DISTANCE;
-    public static final ForgeConfigSpec.DoubleValue RENDER_ALPHA;
-    public static final ForgeConfigSpec.BooleanValue SHOW_MINIMAP;
-    public static final ForgeConfigSpec.BooleanValue SHOW_MATERIAL_LIST;
-
-    public static KeyMapping openGuiKey;
-    public static KeyMapping executePlaceKey;
-    public static KeyMapping toggleRenderKey;
-    public static KeyMapping layerUpKey;
-    public static KeyMapping layerDownKey;
-
-    static {
-        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-        
-        builder.push("Render");
-        RENDER_DISTANCE = builder
-                .comment("Maximum render distance for schematic overlay (blocks)")
-                .defineInRange("renderDistance", 256.0, 16.0, 1024.0);
-        RENDER_ALPHA = builder
-                .comment("Opacity of the schematic overlay (0.0-1.0)")
-                .defineInRange("renderAlpha", 0.4, 0.0, 1.0);
-        SHOW_MINIMAP = builder
-                .comment("Show minimap with build direction")
-                .define("showMinimap", true);
-        SHOW_MATERIAL_LIST = builder
-                .comment("Show material list in HUD")
-                .define("showMaterialList", true);
-        builder.pop();
-        
-        SPEC = builder.build();
-    }
-
-    @SubscribeEvent
-    public static void registerKeys(RegisterKeyMappingsEvent event) {
-        openGuiKey = new KeyMapping(
-                "key.betterschematics.open_gui",
-                GLFW.GLFW_KEY_M,
-                "key.categories.betterschematics"
-        );
-        executePlaceKey = new KeyMapping(
-                "key.betterschematics.execute_place",
-                GLFW.GLFW_KEY_G,
-                "key.categories.betterschematics"
-        );
-        toggleRenderKey = new KeyMapping(
-                "key.betterschematics.toggle_render",
-                GLFW.GLFW_KEY_R,
-                "key.categories.betterschematics"
-        );
-        layerUpKey = new KeyMapping(
-                "key.betterschematics.layer_up",
-                GLFW.GLFW_KEY_UP,
-                "key.categories.betterschematics"
-        );
-        layerDownKey = new KeyMapping(
-                "key.betterschematics.layer_down",
-                GLFW.GLFW_KEY_DOWN,
-                "key.categories.betterschematics"
-        );
-        
-        event.register(openGuiKey);
-        event.register(executePlaceKey);
-        event.register(toggleRenderKey);
-        event.register(layerUpKey);
-        event.register(layerDownKey);
-    }
+    public static final KeyMapping openGuiKey = new KeyMapping("Key.openGui.BetterSchematics", GLFW.KEY_G, "Key.BetterSchematics.Group");
+    public static final KeyMapping executePlaceKey = new KeyMapping("Key.place.BetterSchematics", GLFW.KEY_P, "Key.BetterSchematics.Group");
+    public static final KeyMapping toggleRenderKey = new KeyMapping("Key.toggleRender.BetterSchematics", GLFW.KEY_R, "Key.BetterSchematics.Group");
+    public static final KeyMapping layerUpKey = new KeyMapping("Key.layerUp.BetterSchematics", GLFW.KEY_UP, "Key.BetterSchematics.Group");
+    public static final KeyMapping layerDownKey = new KeyMapping("Key.layerDown.BetterSchematics", GLFW.KEY_DOWN, "Key.BetterSchematics.Group");
+    public static final ForgeConfigSpec SPEC = new ForgeConfigSpec.Builder().comment("Better Schematics Configuration").build();
+    @SubscribeEvent public static void onKeyMappings(RegisterKeyMappingsEvent event) { event.register(openGuiKey); event.register(executePlaceKey); event.register(toggleRenderKey); event.register(layerUpKey); event.register(layerDownKey); }
 }
