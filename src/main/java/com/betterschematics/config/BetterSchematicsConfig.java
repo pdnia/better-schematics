13e1fd2ecd8443f0b803ee84dd0cafd22bb7605e
pdnia <jeszczeraz8976@gmail.com>
2026-07-04 00:39:38 +0200

    Fix BetterSchematicsConfig - clean class

--- src/main/java/com/betterschematics/config/BetterSchematicsConfig.java
+++ src/main/java/com/betterschematics/config/BetterSchematicsConfig.java
@@ -3,8 +3,7 @@ package com.betterschematics.config;
 import com.betterschematics.BetterSchematics;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.jspecify.annotations.NullMarked;
 
-@PublicMarked@nullable    public class BetterSchematicsConfig {
+public class BetterSchematicsConfig {
     public static void registerConfig() {}
 }
