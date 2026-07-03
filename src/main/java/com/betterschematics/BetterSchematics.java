2c8313533bffe2d71936a68f59fae3c31e7eb0f3
pdnia <jeszczeraz8976@gmail.com>
2026-07-04 00:02:02 +0200

    Update BetterSchematics.java - simplified for Forge 1.21.1

--- src/main/java/com/betterschematics/BetterSchematics.java
+++ src/main/java/com/betterschematics/BetterSchematics.java
@@ -2,23 +2,17 @@ package com.betterschematics;
 
 import com.betterschematics.config.BetterSchematicsConfig;
 import com.betterschematics.gui.SchematicScreen;
-import com.betterschematics.render.HUDOverlay;
 import com.betterschematics.render.SchematicRenderer;
 import com.betterschematics.schematic.SchematicManager;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
-import net.minecraftforge.client.event.RenderLevelStageEvent;
-import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(BetterSchematics.MODID)
@@ -29,30 +23,19 @@ public class BetterSchematics {
     private static BetterSchematics instance;
     private final SchematicManager schematicManager;
     private final SchematicRenderer renderer;
-    private final HUDOverlay hudOverlay;
 
     public BetterSchematics() {
         instance = this;
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, BetterSchematicsConfig.SPEC);
-        
         this.schematicManager = new SchematicManager();
         this.renderer = new SchematicRenderer(schematicManager);
-        this.hudOverlay = new HUDOverlay(schematicManager);
-        
-        modEventBus.addListener(this::onClientSetup);
         MinecraftForge.EVENT_BUS.register(this);
+        LOGGER.info("Better Schematics v0.3.0 loaded!");
     }
 
     public static BetterSchematics getInstance() { return instance; }
     public SchematicManager getSchematicManager() { return schematicManager; }
     public SchematicRenderer getRenderer() { return renderer; }
-    public HUDOverlay getHudOverlay() { return hudOverlay; }
-
-    private void onClientSetup(FMLClientSetupEvent event) {
-        LOGGER.info("Better Schematics v0.3.0 loaded!");
-    }
 
     @SubscribeEvent
     public void onKeyInput(InputEvent.Key event) {
@@ -75,26 +58,9 @@ public class BetterSchematics {
     }
 
     @SubscribeEvent
-    public void onRenderWorldLast(RenderLevelStageEvent event) {
-        if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_TRANSLUCENT) {
+    public void onRenderWorld(net.minecraftforge.client.event.RenderLevelStageEvent event) {
+        if (event.getStage() == net.minecraftforge.client.event.RenderLevelStageEvent.Stage.AFTER_TRANSLUCACY_TRANSLUCANTBLOCKS) {
             renderer.render(event.getPoseStack(), event.getCamera(), event.getPartialTick());
-        }
-    }
-
-    /**
-     * Register HUD overlay for minimap, material list, progress bar.
-     */
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onRegisterOverlays(RegisterGuiOverlaysEvent event) {
-            event.registerAbove(VanillaGuiOverlay.CHAT_PANEL.id(), "betterschematics_hud",
-                    (gui, poseStack, partialTick, screenWidth, screenHeight) -> {
-                        BetterSchematics mod = getInstance();
-                        if (mod != null) {
-                            mod.getHudOverlay().render(gui, partialTick.getRealtimeDeltaTicks());
-                        }
-                    });
         }
     }
 }
