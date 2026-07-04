10722493a4a22d1d2439caaf5384194d4f2460cf
pdnia <jeszczeraz8976@gmail.com>
2026-07-04 23:59:21 +0200

    fix: remove broken MinecraftForge.EVENT_BUS.register(), use separate event handler class

--- src/main/java/com/betterschematics/BetterSchematics.java
+++ src/main/java/com/betterschematics/BetterSchematics.java
@@ -5,8 +5,6 @@ import com.betterschematics.gui.SchematicScreen;
 import com.betterschematics.render.HUDOverlay;
 import com.betterschematics.schematic.SchematicManager;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("betterschematics")
@@ -21,33 +19,10 @@ public class BetterSchematics {
         instance = this;
         this.schematicManager = new SchematicManager();
         this.hudOverlay = new HUDOverlay(schematicManager);
-        MinecraftForge.EVENT_BUS.register(this);
+        // Event handlers are registered via @SubscribeEvent in separate class BetterSchematicsEvents
     }
 
     public static BetterSchematics getInstance() { return instance; }
     public SchematicManager getSchematicManager() { return schematicManager; }
     public HUDOverlay getHudOverlay() { return hudOverlay; }
-
-    public void onKeyInput(net.minecraftforge.client.event.InputEvent.Key event) {
-        if (Minecraft.getInstance().player == null) return;
-        while (BetterSchematicsConfig.openGuiKey.consumeClick()) {
-            Minecraft.getInstance().setScreen(new SchematicScreen());
-        }
-        while (BetterSchematicsConfig.executePlaceKey.consumeClick()) {
-            schematicManager.placeNextBlock();
-        }
-        while (BetterSchematicsConfig.toggleRenderKey.consumeClick()) {
-            // toggle render state
-        }
-        while (BetterSchematicsConfig.layerUpKey.consumeClick()) {
-            schematicManager.shiftLayerUp();
-        }
-        while (BetterSchematicsConfig.layerDownKey.consumeClick()) {
-            schematicManager.shiftLayerDown();
-        }
-    }
-
-    public void onClientTick(TickEvent.ClientTickEvent event) {
-        // periodic tasks
-    }
 }
