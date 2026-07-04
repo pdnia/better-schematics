f110034d55d39f8d27ad367e8fa780683d3a44fb
pdnia <jeszczeraz8976@gmail.com>
2026-07-04 18:45:09 +0200

    fix: rewrite BetterSchematics.java for Forge 1.21.11 event system (@SubscribeEvent)

--- src/main/java/com/betterschematics/BetterSchematics.java
+++ src/main/java/com/betterschematics/BetterSchematics.java
@@ -7,20 +7,19 @@ import com.betterschematics.render.SchematicRenderer;
 import com.betterschematics.schematic.SchematicManager;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
-import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.slf4j.Logger;
 
-@Mod(BetterSchematics.MODID)
+@Mod("betterschematics")
 public class BetterSchematics {
     public static final String MODID = "betterschematics";
     public static final Logger LOGGER = LogUtils.getLogger();
@@ -30,16 +29,13 @@ public class BetterSchematics {
     private final SchematicRenderer renderer;
     private final HUDOverlay hudOverlay;
 
-    public BetterSchematics(ModContainer container) {
+    public BetterSchematics() {
         instance = this;
-        container.registerConfig(ModConfig.Type.CLIENT, BetterSchematicsConfig.SPEC);
         this.schematicManager = new SchematicManager();
         this.renderer = new SchematicRenderer(schematicManager);
         this.hudOverlay = new HUDOverlay(schematicManager);
 
-        MinecraftForge.EVENT_BUS.addListener(this::onKeyInput);
-        MinecraftForge.EVENT_BUS.addListener(this::onRenderWorldLast);
-        MinecraftForge.EVENT_BUS.addListener(this::onClientTick);
+        MinecraftForge.EVENT_BUS.addListener(this);
     }
 
     public static BetterSchematics getInstance() { return instance; }
@@ -47,7 +43,8 @@ public class BetterSchematics {
     public SchematicRenderer getRenderer() { return renderer; }
     public HUDOverlay getHudOverlay() { return hudOverlay; }
 
-    private void onKeyInput(InputEvent.Key event) {
+    @SubscribeEvent
+    public void onKeyInput(InputEvent.Key event) {
         if (Minecraft.getInstance().player == null) return;
         while (BetterSchematicsConfig.openGuiKey.consumeClick()) {
             Minecraft.getInstance().setScreen(new SchematicScreen());
@@ -66,11 +63,13 @@ public class BetterSchematics {
         }
     }
 
-    private void onClientTick(net.minecraftforge.event.TickEvent.ClientTickEvent event) {
+    @SubscribeEvent
+    public void onClientTick(net.minecraftforge.event.TickEvent.ClientTickEvent event) {
         // periodic tasks
     }
 
-    private void onRenderWorldLast(RenderLevelStageEvent event) {
+    @SubscribeEvent
+    public void onRenderWorldLast(RenderLevelStageEvent event) {
         if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_TRANSLUCENT) {
             renderer.render(event.getPoseStack(), event.getCamera(), event.getPartialTick());
         }
