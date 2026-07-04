6358f07bf7fb771c0e7c994a1662ed96b72fc3ed
pdnia <jeszczeraz8976@gmail.com>
2026-07-05 00:59:21 +0200

    fix: remove MinecraftForge.EVENT_BUS.register - use IEventBus constructor parameter

--- src/main/java/com/betterschematics/BetterSchematics.java
+++ src/main/java/com/betterschematics/BetterSchematics.java
@@ -1,29 +1,26 @@
 package com.betterschematics;
 
 import com.betterschematics.gui.SchematicOverlay;
-import com.betterschematics.config.BetterSchematicsConfig;
 import com.betterschematics.render.HUDOverlay;
 import com.betterschematics.schematic.SchematicManager;
-import net.minecraftforge.common.MinecraftForge;
+import net.neoforged.bus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 
- @Mod("betterschematics")
- public class BetterSchematics {
-     public static final String MODID = "betterschematics";
+@Mod("betterschematics")
+public class BetterSchematics {
+    public static final String MODID = "betterschematics";
 
-     private static BetterSchematics instance;
-     private final SchematicManager schematicManager;
-     private final HUDOverlay hudOverlay;
+    private static BetterSchematics instance;
+    private final SchematicManager schematicManager;
+    private final HUDOverlay hudOverlay;
 
-     public BetterSchematics() {
-         instance = this;
-         this.schematicManager = new SchematicManager();
-         this.hudOverlay = new HUDOverlay(schematicManager);
-        // Manually register FORGE bus handlers
-        MinecraftForge.EVENT_BUS.register(SchematicOverlay.class);
-     }
+    public BetterSchematics(IEventBus modEventBus) {
+        instance = this;
+        this.schematicManager = new SchematicManager();
+        this.hudOverlay = new HUDOverlay(schematicManager);
+    }
 
-     public static BetterSchematics getInstance() { return instance; }
-     public SchematicManager getSchematicManager() { return schematicManager; }
-     public HUDOverlay getHudOverlay() { return hudOverlay; }
- }
+    public static BetterSchematics getInstance() { return instance; }
+    public SchematicManager getSchematicManager() { return schematicManager; }
+    public HUDOverlay getHudOverlay() { return hudOverlay; }
+}
