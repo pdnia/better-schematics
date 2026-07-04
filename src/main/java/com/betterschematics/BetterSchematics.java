e6aa96c07fa77fe09e68051657438a07b649522b
pdnia <jeszczeraz8976@gmail.com>
2026-07-05 00:56:47 +0200

    fix: register SchematicOverlay manually via MinecraftForge.EVENT_BUS

--- src/main/java/com/betterschematics/BetterSchematics.java
+++ src/main/java/com/betterschematics/BetterSchematics.java
@@ -1,28 +1,29 @@
 package com.betterschematics;
 
+import com.betterschematics.gui.SchematicOverlay;
 import com.betterschematics.config.BetterSchematicsConfig;
-import com.betterschematics.gui.SchematicScreen;
 import com.betterschematics.render.HUDOverlay;
 import com.betterschematics.schematic.SchematicManager;
-import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("betterschematics")
-public class BetterSchematics {
-    public static final String MODID = "betterschematics";
+ @Mod("betterschematics")
+ public class BetterSchematics {
+     public static final String MODID = "betterschematics";
 
-    private static BetterSchematics instance;
-    private final SchematicManager schematicManager;
-    private final HUDOverlay hudOverlay;
+     private static BetterSchematics instance;
+     private final SchematicManager schematicManager;
+     private final HUDOverlay hudOverlay;
 
-    public BetterSchematics() {
-        instance = this;
-        this.schematicManager = new SchematicManager();
-        this.hudOverlay = new HUDOverlay(schematicManager);
-        // Event handlers are registered via @SubscribeEvent in separate class BetterSchematicsEvents
-    }
+     public BetterSchematics() {
+         instance = this;
+         this.schematicManager = new SchematicManager();
+         this.hudOverlay = new HUDOverlay(schematicManager);
+        // Manually register FORGE bus handlers
+        MinecraftForge.EVENT_BUS.register(SchematicOverlay.class);
+     }
 
-    public static BetterSchematics getInstance() { return instance; }
-    public SchematicManager getSchematicManager() { return schematicManager; }
-    public HUDOverlay getHudOverlay() { return hudOverlay; }
-}
+     public static BetterSchematics getInstance() { return instance; }
+     public SchematicManager getSchematicManager() { return schematicManager; }
+     public HUDOverlay getHudOverlay() { return hudOverlay; }
+ }
