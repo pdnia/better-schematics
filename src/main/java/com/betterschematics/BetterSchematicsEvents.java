6815d79adbb6befcc76b0c6ad13ebc932a9c56ec
pdnia <jeszczeraz8976@gmail.com>
2026-07-04 23:59:39 +0200

    feat: add event handler class for key input and client tick

---
+++ src/main/java/com/betterschematics/BetterSchematicsEvents.java
@@ -0,0 +1,40 @@
+package com.betterschematics;
+
+import com.betterschematics.config.BetterSchematicsConfig;
+import com.betterschematics.gui.SchematicScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.event.bus.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+
+@OnlyIn(Dist.CLIENT)
+public class BetterSchematicsEvents {
+
+    @SubscribeEvent
+    public void onKeyInput(InputEvent.Key event) {
+        if (Minecraft.getInstance().player == null) return;
+
+        while (BetterSchematicsConfig.openGuiKey.consumeClick()) {
+            Minecraft.getInstance().setScreen(new SchematicScreen());
+        }
+        while (BetterSchematicsConfig.executePlaceKey.consumeClick()) {
+            BetterSchematics.getInstance().getSchematicManager().placeNextBlock();
+        }
+        while (BetterSchematicsConfig.toggleRenderKey.consumeClick()) {
+            // toggle render state
+        }
+        while (BetterSchematicsConfig.layerUpKey.consumeClick()) {
+            BetterSchematics.getInstance().getSchematicManager().shiftLayerUp();
+        }
+        while (BetterSchematicsConfig.layerDownKey.consumeClick()) {
+            BetterSchematics.getInstance().getSchematicManager().shiftLayerDown();
+        }
+    }
+
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        // periodic tasks
+    }
+}
