059c962c5fc7ae2914e8f383454c88283b49365e
pdnia <jeszczeraz8976@gmail.com>
2026-07-05 11:51:03 +0200

    fix: remove InputEvent.Key from BetterSchematicsEvents, keep only ClientTickEvent and CustomizeGuiOverlayEvent

--- src/main/java/com/betterschematics/BetterSchematicsEvents.java
+++ src/main/java/com/betterschematics/BetterSchematicsEvents.java
@@ -1,13 +1,10 @@
 package com.betterschematics;
 
-import com.betterschematics.config.BetterSchematicsConfig;
-import com.betterschematics.gui.SchematicScreen;
 import com.betterschematics.schematic.SchematicData;
 import com.betterschematics.schematic.SchematicManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
-import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 
@@ -16,27 +13,6 @@ import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
  * in BetterSchematics constructor.
  */
 public class BetterSchematicsEvents {
-
-    @SubscribeEvent
-    public static void onKeyInput(InputEvent.Key event) {
-        if (Minecraft.getInstance().player == null) return;
-
-        while (BetterSchematicsConfig.openGuiKey.consumeClick()) {
-            Minecraft.getInstance().setScreen(new SchematicScreen());
-        }
-        while (BetterSchematicsConfig.executePlaceKey.consumeClick()) {
-            BetterSchematics.getInstance().getSchematicManager().placeNextBlock();
-        }
-        while (BetterSchematicsConfig.toggleRenderKey.consumeClick()) {
-            // toggle render state
-        }
-        while (BetterSchematicsConfig.layerUpKey.consumeClick()) {
-            BetterSchematics.getInstance().getSchematicManager().shiftLayerUp();
-        }
-        while (BetterSchematicsConfig.layerDownKey.consumeClick()) {
-            BetterSchematics.getInstance().getSchematicManager().shiftLayerDown();
-        }
-    }
 
     @SubscribeEvent
     public static void onClientTick(TickEvent.ClientTickEvent event) {
