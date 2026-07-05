a8513f63ef2eb32b7802f6d35b72061b9f107082
pdnia <jeszczeraz8976@gmail.com>
2026-07-05 11:46:34 +0200

    fix: register BetterSchematicsEvents on MinecraftForge.EVENT_BUS via FMLEnvironment.dist check for client side

--- src/main/java/com/betterschematics/BetterSchematics.java
+++ src/main/java/com/betterschematics/BetterSchematics.java
@@ -2,7 +2,10 @@ package com.betterschematics;
 
 import com.betterschematics.render.HUDOverlay;
 import com.betterschematics.schematic.SchematicManager;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod("betterschematics")
 public class BetterSchematics {
@@ -16,6 +19,11 @@ public class BetterSchematics {
         instance = this;
         this.schematicManager = new SchematicManager();
         this.hudOverlay = new HUDOverlay(schematicManager);
+
+        // Register event handlers on the forge bus (only on client side)
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            MinecraftForge.EVENT_BUS.register(BetterSchematicsEvents.class);
+        }
     }
 
     public static BetterSchematics getInstance() { return instance; }
