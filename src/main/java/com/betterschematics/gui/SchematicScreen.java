543e4f0b5fec1391a6e75a9a809995a08d3d9445
pdnia <jeszczeraz8976@gmail.com>
2026-07-07 17:06:21 +0200

    fix: Move file list to left edge (x=20) and controls closer to center

--- src/main/java/com/betterschematics/gui/SchematicScreen.java
+++ src/main/java/com/betterschematics/gui/SchematicScreen.java
@@ -9,17 +9,13 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import com.mojang.logging.LogUtils;
-import org.slf4j.Logger;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SchematicScreen extends Screen {
-    private static final Logger LOGGER = LogUtils.getLogger();
     private final SchematicManager manager;
     private final List<File> schematicFiles = new ArrayList<>();
-    private static final int MAX_FILES_SHOWN = 10;
 
     public SchematicScreen() {
         super(Component.literal("Better Schematics"));
@@ -31,33 +27,38 @@ public class SchematicScreen extends Screen {
         super.init();
         scanSchematicFiles();
         int cx = this.width / 2;
-        int filesX = cx - 200;
-        int filesY = 25;
-        if (!schematicFiles.isEmpty()) {
-            int maxShow = Math.min(schematicFiles.size(), MAX_FILES_SHOWN);
-            for (int i = 0; i < maxShow; i++) {
-                final File file = schematicFiles.get(i);
-                String name = file.getName();
-                if (name.length() > 30) name = name.substring(0, 28) + "..";
-                this.addRenderableWidget(
-                    Button.builder(Component.literal(name), btn -> {
-                        manager.loadSchematic(file);
-                        this.onClose();
-                    }).bounds(filesX, filesY + i * 22, 380, 20).build());
-            }
+        int filesX = 20;
+        int filesY = 35;
+        final int maxShow = Math.min(schematicFiles.size(), 10);
+        for (int i = 0; i < maxShow; i++) {
+            final File file = schematicFiles.get(i);
+            String name = file.getName();
+            if (name.length() > 28) name = name.substring(0, 26) + "..";
+            final String dn = name;
+            this.addRenderableWidget(
+                Button.builder(Component.literal(dn), btn -> {
+                    manager.loadSchematic(file);
+                    this.onClose();
+                }).bounds(filesX, filesY + i * 22, 300, 20).build());
         }
-        int btnW = 90; int btnH = 20; int gap = 24;
-        int ctrlX = cx + 100;
-        int ctrlY = 25;
-        this.addRenderableWidget(Button.builder(Component.literal("Rotate CW"), btn -> manager.rotatePlacement(true)).bounds(ctrlX, ctrlY, btnW, btnH).build());
-        this.addRenderableWidget(Button.builder(Component.literal("Rotate CCW"), btn -> manager.rotatePlacement(false)).bounds(ctrlX + btnW + 5, ctrlY, btnW, btnH).build());
-        ctrlY += gap;
-        this.addRenderableWidget(Button.builder(Component.literal("Mirror X"), btn -> manager.toggleMirror('x')).bounds(ctrlX, ctrlY, btnW, btnH).build());
-        this.addRenderableWidget(Button.builder(Component.literal("Mirror Z"), btn -> manager.toggleMirror('z')).bounds(ctrlX + btnW + 5, ctrlY, btnW, btnH).build());
-        ctrlY += gap;
-        this.addRenderableWidget(Button.builder(Component.literal("Layer +"), btn -> manager.shiftLayerUp()).bounds(ctrlX, ctrlY, btnW, btnH).build());
-        this.addRenderableWidget(Button.builder(Component.literal("Layer -"), btn -> manager.shiftLayerDown()).bounds(ctrlX + btnW + 5, ctrlY, btnW, btnH).build());
-        ctrlY += gap;
+
+        int ctrlX = cx + 60;
+        int ctrlY = 35;
+        final int btnW = 85;
+        final int btnL = 22;
+
+        this.addRenderableWidget(Button.builder(Component.literal("Rotate CW"), btn -> manager.rotatePlacement(true)).bounds(ctrlX, ctrlY, btnW, btnL).build());
+        this.addRenderableWidget(Button.builder(Component.literal("Rotate CCW"), btn -> manager.rotatePlacement(false)).bounds(ctrlX + btnW + 4, ctrlY, btnW, btnL).build());
+        ctrlY += btnL + 4;
+        this.addRenderableWidget(Button.builder(Component.literal("Mirror X"), btn -> manager.toggleMirror('x')).bounds(ctrlX, ctrlY, btnW, btnL).build());
+        this.addRenderableWidget(Button.builder(Component.literal("Mirror Z"), btn -> manager.toggleMirror('z')).bounds(ctrlX + btnW + 4, ctrlY, btnW, btnL).build());
+        ctrlY += btnL + 4;
+        this.addRenderableWidget(Button.builder(Component.literal("Layer +"), btn -> manager.shiftLayerUp()).bounds(ctrlX, ctrlY, btnW, btnL).build());
+        this.addRenderableWidget(Button.builder(Component.literal("Layer -"), btn -> manager.shiftLayerDown()).bounds(ctrlX + btnW + 4, ctrlY, btnW, btnL).build());
+        ctrlY += btnL + 4;
+        this.addRenderableWidget(Button.builder(Component.literal("Layer Mode"), btn -> manager.setLayerMode(!manager.isLayerMode())).bounds(ctrlX, ctrlY, btnW, btnL).build());
+        this.addRenderableWidget(Button.builder(Component.literal("Toggle Render"), btn -> BetterSchematics.getInstance().getRenderer().toggleRender()).bounds(ctrlX + btnW + 4, ctrlY, btnW, btnL).build());
+        ctrlY += btnL + 4;
         this.addRenderableWidget(Button.builder(Component.literal("Materials"), btn -> {
             String m = manager.exportMaterialList();
             Minecraft mc = Minecraft.getInstance();
@@ -66,13 +67,8 @@ public class SchematicScreen extends Screen {
                     if (!line.isBlank()) mc.player.displayClientMessage(Component.literal(line), false);
                 }
             }
-        }).bounds(ctrlX, ctrlY, btnW, btnH).build());
-        this.addRenderableWidget(Button.builder(Component.literal("Toggle Render"), btn -> {
-            BetterSchematics.getInstance().getRenderer().toggleRender();
-        }).bounds(ctrlX + btnW + 5, ctrlY, btnW, btnH).build());
-        ctrlY += gap;
-        this.addRenderableWidget(Button.builder(Component.literal("Layer Mode"), btn -> manager.setLayerMode(!manager.isLayerMode())).bounds(ctrlX, ctrlY, btnW, btnH).build());
-        this.addRenderableWidget(Button.builder(Component.literal("Resume Game"), btn -> this.onClose()).bounds(ctrlX + btnW + 5, ctrlY, btnW, btnH).build());
+        }).bounds(ctrlX, ctrlY, btnW, btnL).build());
+        this.addRenderableWidget(Button.builder(Component.literal("Resume Game"), btn -> this.onClose()).bounds(ctrlX + btnW + 4, ctrlY, btnW, btnL).build());
     }
 
     private void scanSchematicFiles() {
@@ -91,7 +87,7 @@ public class SchematicScreen extends Screen {
         super.render(g, mx, my, partialTick);
         g.drawCenteredString(this.font, Component.literal("Better Schematics v0.3"), this.width / 2, 8, 0xFFFFFFFF);
         if (schematicFiles.isEmpty()) {
-            g.drawCenteredString(this.font, Component.literal("No .litematic files in schematics/"), this.width / 2, this.height / 2, 0xFFFFFFFF);
+            g.drawCenteredString(this.font, Component.literal("No .litematic files in schematics/"), this.width / 2, 20, 0xFFFFFFFF);
         }
         if (manager.hasSchematic()) {
             SchematicData d = manager.getActiveSchematic();
