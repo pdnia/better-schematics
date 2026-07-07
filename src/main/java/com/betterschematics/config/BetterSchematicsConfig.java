1d1a5834eb7a37e60c22b51c9a7b388be75d5a07
pdnia <jeszczeraz8976@gmail.com>
2026-07-07 21:55:42 +0200

    fix: GLFV_KEY_R -> GLFW_KEY_R

--- src/main/java/com/betterschematics/config/BetterSchematicsConfig.java
+++ src/main/java/com/betterschematics/config/BetterSchematicsConfig.java
@@ -6,11 +6,7 @@ import net.minecraft.resources.Identifier;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import org.lwjgl.glfw.GLFW;
 
-/**
- * Mod configuration and key binding definitions.
- */
 public class BetterSchematicsConfig {
-
     public static final KeyMapping.Category CATEGORY = new KeyMapping.Category(Identifier.fromNamespaceAndPath("betterschematics", "keycategory"));
 
     public static KeyMapping openGuiKey;
@@ -20,7 +16,7 @@ public class BetterSchematicsConfig {
 
     public static void registerKeys(RegisterKeyMappingsEvent event) {
         openGuiKey = new KeyMapping("betterschematics.key.open_gui", GLFW.GLFW_KEY_M, CATEGORY);
-        toggleRenderKey = new KeyMapping("betterschematics.key.toggle_render", GLFW.GLFV_KEY_R, CATEGORY);
+        toggleRenderKey = new KeyMapping("betterschematics.key.toggle_render", GLFW.GLFW_KEY_R, CATEGORY);
         layerUpKey = new KeyMapping("betterschematics.key.layer_up", GLFW.GLFW_KEY_UP, CATEGORY);
         layerDownKey = new KeyMapping("betterschematics.key.layer_down", GLFW.GLFW_KEY_DOWN, CATEGORY);
         event.register(openGuiKey);
