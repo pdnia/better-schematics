efe02f1d3222cd4970f1ae4eb9ede35dca9d3d63
pdnia <jeszczeraz8976@gmail.com>
2026-07-08 15:12:51 +0200

    remove executePlaceKey (G key) - blocks will be placed manually

--- src/main/java/com/betterschematics/config/BetterSchematicsConfig.java
+++ src/main/java/com/betterschematics/config/BetterSchematicsConfig.java
@@ -6,7 +6,11 @@ import net.minecraft.resources.Identifier;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import org.lwjgl.glfw.GLFW;
 
+/**
+ * Mod configuration and key binding definitions.
+ */
 public class BetterSchematicsConfig {
+
     public static final KeyMapping.Category CATEGORY = new KeyMapping.Category(Identifier.fromNamespaceAndPath("betterschematics", "keycategory"));
 
     public static KeyMapping openGuiKey;
@@ -14,6 +18,9 @@ public class BetterSchematicsConfig {
     public static KeyMapping layerUpKey;
     public static KeyMapping layerDownKey;
 
+    /**
+     * Registers key mappings. Called during RegisterKeyMappingsEvent.
+     */
     public static void registerKeys(RegisterKeyMappingsEvent event) {
         openGuiKey = new KeyMapping("betterschematics.key.open_gui", GLFW.GLFW_KEY_M, CATEGORY);
         toggleRenderKey = new KeyMapping("betterschematics.key.toggle_render", GLFW.GLFW_KEY_R, CATEGORY);
